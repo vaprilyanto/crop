fd42ec91867327b587623871d7319e99c8559a9b
vaprilyanto <47765445+vaprilyanto@users.noreply.github.com>
2020-02-03 09:26:20 +0700

    Update main_guide_align.py
    Added Hsu and CFD scores for individual guide-variant pair.

--- main_guide_align.py
+++ main_guide_align.py
@@ -19,7 +19,7 @@ with open(INIT_SEQ, 'r') as init_seq:
   for (name, seq), map_file in zip(read_fasta(init_seq), range(1, MAP_NUMBER+1)):
     query_seq = codecs.open('%smap_s%d.txt' % (map_path, map_file), 'r')
     align_sfile = codecs.open(align_path + name[1:] + '_align.csv', 'w')
-    align_sfile.write('GUIDE TO VARIANT ALIGN FILE FOR: %s\n\nName,Guide,Variant,MM_Guide,MM_PAM,Alignment\n' % INIT_SEQ.rsplit('/',1)[1])
+    align_sfile.write('GUIDE TO VARIANT ALIGN FILE FOR: %s\n\nName,Guide,Variant,MM_Guide,MM_PAM,Alignment,Hsu,CFD\n' % INIT_SEQ.rsplit('/',1)[1])
 
     # reading map file row-wise
     rows = csv.reader(query_seq, delimiter = '\t')
@@ -29,6 +29,8 @@ with open(INIT_SEQ, 'r') as init_seq:
     mm_guides = []
     mm_pams = []
     aligns = []
+    hsu_list = []
+    cfd_list = []
 
     for row in rows:
       if 'subs' in row[0]:
@@ -39,7 +41,9 @@ with open(INIT_SEQ, 'r') as init_seq:
         aligns.append(''.join(['.' if i == j else '|' for i, j in zip(seq,row[4])]))
         mm_guides.append(sum(seq != var for seq, var in zip(seq[:20], row[4][:20]))) # count mismatch between 20-nt seqs
         mm_pams.append(sum(seq != var for seq, var in zip(seq[20:], row[4][20:]))) # count mismatch between PAMs
+        hsu_list.append(float(calc_hsu_score(seq[:20], row[4][:20])))
+        cfd_list.append(float(calc_cfd(seq[:20], row[4][:20], row[4][-2:])))
 
-    for guide_var, mm_guide, mm_pam, align in zip(guide_vars, mm_guides, mm_pams, aligns):      
-      align_sfile.write('%s,%s,%s,%d,%d,%s\n' % (name[1:], seq, guide_var, mm_guide, mm_pam, align))
+    for guide_var, mm_guide, mm_pam, align, hsu, cfd in zip(guide_vars, mm_guides, mm_pams, aligns, hsu_list, cfd_list):      
+      align_sfile.write('%s,%s,%s,%d,%d,%s,%s,%s\n' % (name[1:], seq, guide_var, mm_guide, mm_pam, align, str(hsu), str(cfd)))
     align_sfile.close()
